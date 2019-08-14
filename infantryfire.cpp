4f4d6a77bb92cfff131035d2096ac4e521efcd41
erickveil <erick@erickveil.net>
2019-08-14 15:21:43 -0700

    Re-arranged the tables... again.
    Trying to break down the dice faces, when it's already been done in
    Reisswitz's book, started to seem silly to me.
    
    So now I'm starting with the CRTs and as a result the inputs are
    way more organized.

---
+++ infantryfire.cpp
@@ -0,0 +1,6 @@
+#include "infantryfire.h"
+
+InfantryFire::InfantryFire()
+{
+
+}
---
+++ infantryfire.h
@@ -0,0 +1,11 @@
+#ifndef INFANTRYFIRE_H
+#define INFANTRYFIRE_H
+
+
+class InfantryFire
+{
+public:
+    InfantryFire();
+};
+
+#endif // INFANTRYFIRE_H

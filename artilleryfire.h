435a5874991664dba1c5096347de1724ca684939
eveil <erick@erickveil.net>
2019-08-30 16:15:37 -0700

    Attempting to work out artillery dice
    The 6 and 7 lb artillery appear to be represented on the dice, but the
    10 and 12 lb cannons have table values that don't match the dice, nor is
    there any formula provided to indicate what they're derived from.

--- artilleryfire.h
+++ artilleryfire.h
@@ -2,6 +2,13 @@
 #define ARTILLERYFIRE_H
 
 
+/**
+ * @brief The ArtilleryFire class
+ * 12 lb battery g(9, 13, 31, 50) - b(3, 7, 20, 31) - appears tp be table only?
+ * 6 lb battery g(7, 10, 25, 40) Die 3, left col - b(3, 6, 16, 25) Die 5, left col
+ * 7 lb howitzer g(10, 12, 20, 30) Die 3, right col - b(3, 8, 12, 20) Die 5, right col
+ * two 10 lb Howitzers g(3, 4, 6, 9) - b(1, 2, 4, 6) - appears to be table only?
+ */
 class ArtilleryFire
 {
 public:
